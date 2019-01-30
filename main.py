d2a7af90c5385daf43a3f461d4b7c1a984dd49f7
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-01-30 16:27:16 +0300

    print("Done!")

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ GA.generate_pop()
 
 GA.geneLen = 50
 # Set set of possible values for alleles.
-GA.phenotype = list(range(0, 11))
+GA.phenotype = list(range(0, 2))
 
 # Set target fitness based on maximum possible.
 targetFitness = max(GA.phenotype)*GA.geneLen
 # Start genetic magic.
 GA.loop(targetFitness)
+print("Done!")
 
 print(f"Best fitness: {GA.best_fitness()}")
 print(f"Worst fitness: {GA.worst_fitness()}")
